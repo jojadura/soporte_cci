e7ffea8a252bc63eb4ba2431f465551134217d84
John Jairo <jojadura@gmail.com>
2016-08-09 10:32:51 -0500

    Archivo seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,34 +5,33 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-    Role.find_or_create_by(:name=>"Administrador")
-    puts "Se creo Role Administrador"
-    Role.find_or_create_by(:name=>"Director")
-    puts "Se creo Role  Director de Area"
-    Role.find_or_create_by(:name=>"Usuario")
-    puts "Se creo Role Usuario"
-    User.create!({
-    	:email=>'admin@gmmail.com',
-    	:password=> 'changeme',
-        :sign_in_count=>0,
-    	:encrypted_password=> User.new(:password => 'changeme').encrypted_password,
-    	:name=> 'Administrador',
-    	:role_id => Role.admin.id,
-    	:created_at=> Time.now.strftime("%Y-%m-%d"),
-        :updated_at=> Time.now.strftime("%Y-%m-%d"),
-         })
-    puts "se creo el usuario ADMINISTRADOR" 
-    puts ">>>>>>>>>>   USER:  admin@gmail.com"
-    puts ">>>>>>>>>PASSWORD:  changeme"
 
-    State.find_or_create_by(:name=>"Abierto",:color=>"#e31919")
-     puts "Se creo State Abierto"
-    
-    State.find_or_create_by(:name=>"Pendiente",:color=>"#f9f822")
-     puts "Se creo State Pendiente"
-    
-    State.find_or_create_by(:name=>"Pre-finalizado",:color=>"#1148cc")
-     puts "Se creo State Pre-finalizado"
-    
-    State.find_or_create_by(:name=>"Finalizado",:color=>"#20c807")
-     puts "Se creo State Finalizado"
+Direction.create!({
+    :name=>'Dirección Jurídica',
+    :created_at=> Time.now.strftime("%Y-%m-%d"),
+    :updated_at=> Time.now.strftime("%Y-%m-%d"),
+ })
+
+Direction.create!({
+    :name=>'Dirección Competitividad e Innovación',
+    :created_at=> Time.now.strftime("%Y-%m-%d"),
+    :updated_at=> Time.now.strftime("%Y-%m-%d"),
+ })
+
+Direction.create!({
+    :name=>'Dirección Comercial y Mercadeo',
+    :created_at=> Time.now.strftime("%Y-%m-%d"),
+    :updated_at=> Time.now.strftime("%Y-%m-%d"),
+ })
+
+Direction.create!({
+    :name=>'Dirección Administrativa y Financiera',
+    :created_at=> Time.now.strftime("%Y-%m-%d"),
+    :updated_at=> Time.now.strftime("%Y-%m-%d"),
+ })
+
+Direction.create!({
+    :name=>'Dirección Investigaciones y Publicidad',
+    :created_at=> Time.now.strftime("%Y-%m-%d"),
+    :updated_at=> Time.now.strftime("%Y-%m-%d"),
+ })
